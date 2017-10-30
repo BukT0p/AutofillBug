488dd42ebde2ff84d09f02e98117ffe8205bd3c4
BukT0p <yak_vick@mail.ru>
2017-10-30 21:30:42 +0200

    Added fix from here https://stackoverflow.com/a/47021399/2290950

--- app/src/main/kotlin/com/vyakunin/KotlinTestActivity.kt
+++ app/src/main/kotlin/com/vyakunin/KotlinTestActivity.kt
@@ -6,14 +6,12 @@ import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.vyakunin.kotlintests.R
-import kotlinx.android.synthetic.main.activity_kotlin_test.*
 
 
 class KotlinTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_test)
-        label.text = "Enable autofill -> long press on each input field to trigger 'autofill' popup -> select autofill -> fails on TextInputEditText"
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             disableAutoFill()
         }
